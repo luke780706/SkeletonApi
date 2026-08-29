bdd4b6fe8ba0064bd48d8a5096394729bd82352b
luke780706 <luke78@interia.pl>
2026-08-29 12:58:31 +0200

    Add service unit tests and DTO validation

--- DTOs/ProductDto.cs
+++ DTOs/ProductDto.cs
@@ -1,9 +1,13 @@
-﻿namespace SkeletonApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkeletonApi.DTOs
 {
     public class ProductDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Range(0.01,double.MaxValue)]
         public decimal Price { get; set; }
     }
 }
--- DTOs/UpdateProductDto.cs
+++ DTOs/UpdateProductDto.cs
@@ -1,8 +1,12 @@
-﻿namespace SkeletonApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkeletonApi.DTOs
 {
     public class UpdateProductDto
     {
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Range(0.01, double.MaxValue)]
         public decimal Price { get; set; }
     }
 }
